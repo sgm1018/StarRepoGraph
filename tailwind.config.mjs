4a2fe8db77a0978336a2f87300250443bd6b0bcc
Sergio González Martínez <1018sgm1018@gmail.com>
2025-01-11 14:57:06 +0100

    Feat: UI added

---
+++ tailwind.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        accent: 'var(--accent)',
+        background: 'var(--background)',
+        text: 'var(--text)',
+        border: 'var(--border)',
+        blob: 'var(--blob)'
+      },
+      animation: {
+        blob: 'blob 7s infinite'
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)'
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)'
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)'
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)'
+          }
+        }
+      }
+    }
+  }
+};
