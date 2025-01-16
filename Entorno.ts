2d211f1fa8267044e612e812a28f08c26c2a57c7
Sergio González Martínez <1018sgm1018@gmail.com>
2025-01-16 22:08:01 +0100

    Feat: changed section searchbar subtitle

--- Entorno.ts
+++ Entorno.ts
@@ -1,4 +1,4 @@
 export const Entorno = {
     Api: 'https://api.github.com',
-    Token: ''
+    Token: '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'
 }
