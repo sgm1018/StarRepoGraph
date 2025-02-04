a42c066b5c134e69707014984659272424fe093e
sergio.gonzalez <sergio.gonzalez@tchlabs.eu>
2025-02-04 10:33:15 +0100

    Feat: new Vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { Entorno } from './Entorno';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
+
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
   port: Entorno.port,
   host: Entorno.host,
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
